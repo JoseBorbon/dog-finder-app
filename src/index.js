3729d9f2fa68e4d57345d19e09b905dc150785fc
JoseBorbon <joserborbon@gmail.com>
2021-11-04 08:09:27 -0400

    add base name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dogs">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
