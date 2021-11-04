4e873e3cf8a4137d9c0818531975e3942c9584d3
JoseBorbon <joserborbon@gmail.com>
2021-11-04 12:26:56 -0400

    refactor app component

--- src/App.js
+++ src/App.js
@@ -1,89 +1,56 @@
-import './App.css';
+import React, { Component } from 'react';
+import Routes from './Routes';
+import Nav from './Nav';
+import hazel from './assets/images/hazel.jpg';
+import tubby from './assets/images/tubby.jpg';
+import whiskey from './assets/images/whiskey.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: 'Whiskey',
+        age: 5,
+        src: whiskey,
+        facts: [
+          'Whiskey loves eating popcorn.',
+          'Whiskey is a terrible guard dog.',
+          'Whiskey wants to cuddle with you!',
+        ],
+      },
+      {
+        name: 'Hazel',
+        age: 3,
+        src: hazel,
+        facts: [
+          'Hazel has soooo much energy!',
+          'Hazel is highly intelligent.',
+          'Hazel loves people more than dogs.',
+        ],
+      },
+      {
+        name: 'Tubby',
+        age: 4,
+        src: tubby,
+        facts: [
+          'Tubby is not the brightest dog',
+          'Tubby does not like walks or exercise.',
+          'Tubby loves eating food.',
+        ],
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav dogs={this.props.dogs} />
+        <div className="container">
+          <Routes dogs={this.props.dogs} />
         </div>
-      </nav>
-      <h1 className="display-1">Dog App</h1>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
