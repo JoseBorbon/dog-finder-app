c12604165b25c8d8852b48776dddbefdcf4c1c2a
JoseBorbon <joserborbon@gmail.com>
2021-11-04 12:29:53 -0400

    add Routes component

---
+++ src/Routes.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import DogList from './DogList';
+import DogDetails from './DogDetails';
+
+export default class Routes extends Component {
+  render() {
+    const getDog = (props) => {
+      let currentDog = this.props.dogs.find(
+        (dog) =>
+          dog.name.toLowerCase() === props.match.params.name.toLowerCase()
+      );
+
+      return <DogDetails {...props} dog={currentDog} />;
+    };
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <DogList dogs={this.props.dogs} />}
+        />
+        <Route exact path="/:name" render={getDog} />
+        <Redirect exact to="/" />
+      </Switch>
+    );
+  }
+}
